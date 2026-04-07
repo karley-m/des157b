009d1edd03c2d89975f5bfba685d6902f299d67a
karley-m <kbmakar@ucdavis.edu>
2026-04-07 07:05:57 -0700

    portal

--- script.js
+++ script.js
@@ -3,23 +3,27 @@
 
     const button = document.querySelector('button');
     const body = document.querySelector('body');
-    const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
+    const main = document.querySelector('main');
+    const header = document.querySelector('header');
+    const phrase = document.querySelector('#phrase');
     let mode = 'dark';
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
             body.className = 'switch';
-            banner.className = 'switch';
             button.className = 'switch';
+            main.className = 'switch'
+            header.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
             mode = 'light';
         } else {
             body.removeAttribute('class');
-            banner.removeAttribute('class');
             button.removeAttribute('class');
+            main.removeAttribute('class');
+            header.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
