7a37ad8b55e9853c09b218417d7998b1df36b8d8
karley-m <kbmakar@ucdavis.edu>
2026-04-07 18:31:31 -0700

    bg & font change

--- script.js
+++ script.js
@@ -6,15 +6,19 @@
     const sections = document.querySelectorAll('section')
     const main = document.querySelector('main');
     const header = document.querySelector('header');
-    const phrase = document.querySelector('#phrase');
+    const h2 = document.querySelector('h2');
+    const h1 = document.querySelector('h1');
     let mode = 'dark';
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
             body.className = 'switch';
             button.className = 'switch';
-            main.className = 'switch'
+            button.innerHTML = "<img src='images/lighton.png' width='50'>";
+            main.className = 'switch';
             header.className = 'switch';
+            h2.className = 'switch';
+            h1.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -22,8 +26,11 @@
         } else {
             body.removeAttribute('class');
             button.removeAttribute('class');
+            button.innerHTML = "<img src='images/lightoff.png' width='50'>";
             main.removeAttribute('class');
             header.removeAttribute('class');
+            h2.removeAttribute('class');
+            h1.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
